f4d31d960547786e2162d23f2124632610c8054f
Việt An <vietan00892b@gmail.com>
2023-07-28 11:29:04 +0700

    feat: stop using as proxy, allow cors, fix timeout error

--- index.ts
+++ index.ts
@@ -1,34 +1,73 @@
+import http from 'node:http';
+import https from 'node:https';
 import express from 'express';
 import cheerio from 'cheerio';
 import fetch from 'node-fetch';
+import cors from 'cors';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const allowlist = ['http://localhost:5173', 'https://sticky-md.web.app'];
+const corsOptionsDelegate = function (req: any, callback: any) {
+  let corsOptions;
+  if (allowlist.indexOf(req.header('Origin')) !== -1) {
+    corsOptions = { origin: true }; // reflect (enable) the requested origin in the CORS response
+  } else {
+    corsOptions = { origin: false }; // disable CORS for this request
+  }
+  callback(null, corsOptions); // callback expects two parameters: error and options
+};
+
+const AbortController = globalThis.AbortController;
+
+const controller = new AbortController();
+const timeout = setTimeout(() => {
+  console.log('Aborting after 30s');
+  controller.abort();
+}, 30000);
+
+const httpAgent = new http.Agent({ keepAlive: true });
+const httpsAgent = new https.Agent({ keepAlive: true });
+const agentSelector = function (_parsedURL: any) {
+  if (_parsedURL.protocol == 'http:') {
+    return httpAgent;
+  } else {
+    return httpsAgent;
+  }
+};
+
 async function fetchStuffs(url: string) {
-  const res = await fetch(url);
-  const html = await res.text();
-  const $ = cheerio.load(html);
+  try {
+    await new Promise((resolve) => setTimeout(resolve, 0));
+    const res = await fetch(url, { agent: agentSelector, timeout: 30000 });
+    const html = await res.text();
+    const $ = cheerio.load(html);
 
-  const getMetatag = (name: string) =>
-    $(`meta[name=${name}]`).attr('content') ||
-    $(`meta[name="og:${name}"]`).attr('content') ||
-    $(`meta[name="twitter:${name}"]`).attr('content') ||
-    $(`meta[property="og:${name}"]`).attr('content');
+    const getMetatag = (name: string) =>
+      $(`meta[name=${name}]`).attr('content') ||
+      $(`meta[name="og:${name}"]`).attr('content') ||
+      $(`meta[name="twitter:${name}"]`).attr('content') ||
+      $(`meta[property="og:${name}"]`).attr('content');
 
-  const faviconRelative =
-    $('link[rel="shortcut icon"]').attr('href') || $('link[rel="icon"]').attr('href');
-  const faviconFull = new URL(url).origin + faviconRelative;
+    const faviconRelative =
+      $('link[rel="shortcut icon"]').attr('href') || $('link[rel="icon"]').attr('href');
+    const faviconFull = new URL(url).origin + faviconRelative;
 
-  const info = {
-    url,
-    title: $('title').first().text(),
-    favicon: faviconFull,
-    description: getMetatag('description'),
-    image: getMetatag('image'),
-    author: getMetatag('author'),
-  };
-  return info;
+    const info = {
+      url,
+      title: $('title').first().text(),
+      favicon: faviconFull,
+      description: getMetatag('description'),
+      image: getMetatag('image'),
+      author: getMetatag('author'),
+    };
+    return info;
+  } catch (err) {
+    console.log(err);
+  } finally {
+    clearTimeout(timeout);
+  }
 }
 
 app.get('/', (req, res) => {
@@ -38,7 +77,7 @@ app.get('/', (req, res) => {
   });
 });
 
-app.get('/api', async (req, res) => {
+app.get('/api', cors(corsOptionsDelegate), async (req, res) => {
   const url = req.query.url as string;
   const data = await fetchStuffs(url);
   console.log(data);
